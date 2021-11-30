5e2c9d91b5d57a41bc98d3f6ce89204304497732
vedroboev <ftabaev_1@edu.hse.ru>
2021-11-30 19:04:02 +0300

    Add sorting module for container

---
+++ Source/heapsort.c
@@ -0,0 +1,60 @@
+#include "container.h"
+#include "todouble.h"
+#include "sharedconstants.h"
+
+// Swaps the two numbers in the container located at specified pointers.
+void swap(void* first, void* second){
+    // Converting pointers to char.
+    unsigned char* first_number = first;
+    unsigned char* second_number = second;
+
+    // Swapping two numbers located at these pointers.
+    for (int i = 0; i < number_size; ++i) {
+        unsigned char temp = *(first_number + i);
+        *(first_number + i) = *(second_number + i);
+        *(second_number + i) = temp;
+    }
+}
+
+// Heapifies array at the specified point (element at pointer). Index is passed as a byte shift from array beginning.
+void generateHeap(void* container, int index, int length){
+    // Getting largest, left and right as shifts from container beginning.
+    int largest = index;
+    int left = 2 * index + number_size;
+    int right = left + number_size;
+
+    int byte_length = length * number_size;
+
+    // Finding true largest element.
+    if(left < byte_length){
+        if(numberToDouble(container + index) < numberToDouble(container + left)){
+            largest = left;
+        }
+    }
+    if(right < byte_length){
+        if(numberToDouble(container + right) > numberToDouble(container + largest)){
+            largest = right;
+        }
+    }
+
+    // If largest element wasn't truly the largest, we swap it with element at index and try again.
+    if(largest != index){
+        swap(container + largest, container + index);
+        generateHeap(container, largest, length);
+    }
+}
+
+void heapSort(void* container, int length){
+    // Generating a full heap in our container.
+    for (int i = length / 2 - 1; i >= 0; --i) {
+        generateHeap(container, i * number_size, length);
+    }
+
+    // Extracting heap elements in correct order, generating sorted container.
+    for(int i = length - 1; i >= 0; --i){
+        // Moving root to the index.
+        swap(container, container + i * number_size);
+        // Heapifying the partial heap.
+        generateHeap(container, 0, i);
+    }
+}
